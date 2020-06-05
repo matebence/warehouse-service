c32f6c3247306a0e7b80529328ec5e70ab01ae0c
Bence <m.bence05@gmail.com>
2020-06-05 13:23:08 +0200

    wait-for-it script Dockerfile a boostrap.js subor

---
+++ resources/bootstrap.js
@@ -0,0 +1,18 @@
+module.exports = {
+    application: {
+        name: "warehouses-service"
+    },
+    cloud: {
+        config: {
+            uri: "http://192.168.99.100:8888",
+            username: "app-blesk-config-server",
+            password: "b8199f18ee07292f39f5d9213cf493e8"
+        }
+    },
+    profiles: {
+        active: "dev"
+    },
+    server: {
+        port: 5400
+    }
+};
