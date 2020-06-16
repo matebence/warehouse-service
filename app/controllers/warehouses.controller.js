08b058c684bfccd151dbcfddbf1132c26d128dd7
Bence <m.bence05@gmail.com>
2020-06-16 20:03:18 +0200

    vetva pre mongoid search a regex

--- app/controllers/warehouses.controller.js
+++ app/controllers/warehouses.controller.js
@@ -402,7 +402,11 @@ exports.search = {
         }
         if (search) {
             Object.keys(search).map(function (key, index) {
-                search[key] = {$regex: new RegExp("^.*" + search[key] + '.*', "i")}
+                if(isNaN(search[key]) &&  new RegExp("^[0-9a-fA-F]{24}$").test(search[key])){
+                    search[key] = database.mongoose.Types.ObjectId(search[key])
+                } else if (isNaN(search[key])){
+                    search[key] = {$regex: new RegExp("^.*" + search[key] + '.*', "i")}
+                }
             });
         }
         Warehouses.countDocuments({deleted: false, ...search}, (err, count) => {
